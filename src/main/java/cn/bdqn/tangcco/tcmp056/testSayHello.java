254670500f2c62dcb83e2ba55ab51fa7fe34a206
13651227037 <929265369@qq.com>
2017-08-03 18:50:33 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/testSayHello.java
@@ -0,0 +1,13 @@
+package cn.bdqn.tangcco.tcmp056;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/8/3.
+ */
+public class testSayHello {
+    @Test
+    public void testSayHello() {
+        System.out.println("hello world");
+    }
+}
